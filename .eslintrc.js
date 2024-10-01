1f3c2ff8b026d3eeedee7f44b166ccd82207e1c4
shaheer shakir <shaheershakir22@gmail.com>
2024-10-01 08:18:57 +0530

    add zustand for onboarding state managment

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier", "react-native"],
   rules: {
-    "prettier/prettier": "error",
+    // "prettier/prettier": "error",
     "react-native/no-unused-styles": "error",
   },
 };
--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,22 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import { theme } from "@/theme";
+import { useUserStore } from "@/store/userStore";
+import { useRouter } from "expo-router";
 
 export default function OnboardingScreen() {
+  const router = useRouter();
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHadOnboarded);
+  const handlePress = () => {
+    toggleHasOnboarded();
+    router.replace("/");
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Onboarding</Text>
+      <Button title="Let me in" onPress={handlePress} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
-  },
-  text: {
-    fontSize: 24,
   },
 });
